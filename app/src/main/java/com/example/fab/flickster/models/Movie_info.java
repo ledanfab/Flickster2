5b2c92b831ab4fc01260dcfd36f4403b10acf746
Fab <ledanfab.93@gmail.com>
2018-07-10 22:43:53 -0800

    First Version

---
+++ app/src/main/java/com/example/fab/flickster/models/Movie_info.java
@@ -0,0 +1,15 @@
+package com.example.fab.flickster.models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fab.flickster.R;
+
+public class Movie_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_info);
+    }
+}
